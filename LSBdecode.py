1cef178a9d7de86d9fcde73d5e1411123f6bb248
NihilistPenguin <56986124+NihilistPenguin@users.noreply.github.com>
2020-03-10 18:01:17 -0400

    Update LSBdecode.py

--- LSBdecode.py
+++ LSBdecode.py
@@ -17,14 +17,12 @@ for x in range(0, image.width):
 	extracted += bin(b)[-1]
 
 chars = []
-#print(extracted[0:160])
+
 for i in range(0, len(extracted)/8):
 	#byte from 0-8, 8-16, ...
 	byte = extracted[i*8:(i+1)*8]
 	#make string of each bit in a byte, convert to decimal int, convert to char
 	chars.append(chr(int(''.join([str(bit) for bit in byte]), base=2)))
-
-#print(chars)
 
 #join chars in array into a string and decode
 flag = base64.b64decode(''.join(chars) + "==")
