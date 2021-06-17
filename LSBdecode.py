902afd205bfd0fe41ca3c627683df948e49b1487
Daniel Getter <56986124+NihilistPenguin@users.noreply.github.com>
2021-06-17 19:07:42 -0400

    Update LSBdecode.py

--- LSBdecode.py
+++ LSBdecode.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python3
+
 import base64
 import sys
 from PIL import Image
+import string
+import argparse
 
-input_image = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("image", help="steg image filename")
+args = parser.parse_args()
+
+input_image = args.image
 image = Image.open(input_image)
 
 extracted = ''
@@ -18,13 +26,16 @@ for x in range(0, image.width):
 
 chars = []
 
-for i in range(0, len(extracted)/8):
-	#byte from 0-8, 8-16, ...
+for i in range(0, int(len(extracted)/8)):
+	# byte from 0-8, 8-16, ...
 	byte = extracted[i*8:(i+1)*8]
-	#make string of each bit in a byte, convert to decimal int, convert to char
-	chars.append(chr(int(''.join([str(bit) for bit in byte]), base=2)))
+	# make string of each bit in a byte, convert to decimal int, convert to char
+	c = chr(int(''.join([str(bit) for bit in byte]), base=2))
+	if c in string.printable:
+		chars.append(c)
 
-#join chars in array into a string and decode
+
+# join chars in array into a string and decode
+# print(chars)
 flag = base64.b64decode(''.join(chars) + "==")
 print(flag)
-
